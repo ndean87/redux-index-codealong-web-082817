13e0012f8ab0886912ccffa4e4400e5a1a9a16b5
ndean87 <ndean87@gmail.com>
2017-11-17 10:23:35 -0500

    Done.

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -1,19 +1,21 @@
+
 import React, { Component } from 'react';
+import Todo from './Todo'; /* code changed */
 
 class Todos extends Component {
 
-  render() {
+ render() {
 
-    const todos = this.props.store.getState().todos.map((todo, index) => {
-      return <li key={index}>{todo.text}</li>
-    });
+   const todos = this.props.store.getState().todos.map((todo, index) => {
+     return <Todo text={todo.text} key={index} /> /* code changed */
+   });
 
-    return(
-      <ul>
-        {todos}
-      </ul>
-    );
-  }
+   return(
+     <ul>
+       {todos}
+     </ul>
+   );
+ }
 };
 
 export default Todos;
